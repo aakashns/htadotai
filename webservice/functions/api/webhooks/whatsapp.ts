38038986e069fd830c15cf07de91119fcc9f2775
htadotai <144734864+htadotai@users.noreply.github.com>
2023-09-20 01:07:44 +0000

    parse params separately

--- webservice/functions/api/webhooks/whatsapp.ts
+++ webservice/functions/api/webhooks/whatsapp.ts
@@ -4,17 +4,20 @@ import { WhatsAppWebhookBody } from "@/lib/whatsapp";
 export async function onRequestGet(context: Context) {
   const config = getConfig(context);
   const { request, waitUntil } = context;
-  const { search, searchParams } = new URL(request.url);
-
-  console.log("Setting up whatsapp webhook", {
-    search,
-    searchParams,
-    url: request.url,
-  });
+  const { search } = new URL(request.url);
+  const searchParams = new URLSearchParams(search);
 
   let mode = searchParams.get("hub.mode");
   let token = searchParams.get("hub.verify_token");
   let challenge = searchParams.get("hub.challenge");
+
+  console.log("Setting up whatsapp webhook", {
+    search,
+    url: request.url,
+    mode,
+    token,
+    challenge,
+  });
 
   if (mode === "subscribe" && token === config.WHATSAPP_WEBHOOK_SECRET) {
     return new Response(challenge, { status: 200 });
