38d0db40997f4ce7de5336744c3e72ec7e2b5159
htadotai <144734864+htadotai@users.noreply.github.com>
2023-09-20 01:30:38 +0000

    create headers object

--- webservice/functions/api/webhooks/whatsapp.ts
+++ webservice/functions/api/webhooks/whatsapp.ts
@@ -18,10 +18,19 @@ export async function onRequestGet(context: Context) {
 export async function onRequestPost(context: Context) {
   const config = getConfig(context);
   const { request, waitUntil } = context;
+  const headers: any = {};
+
+  for (let [key, value] of Array.from(request.headers.entries())) {
+    headers[key] = value;
+  }
 
   // implement stuff here
   const requestBody = await request.json<WhatsAppWebhookBody>();
-  console.log("WhatsApp webhook received", { url: request.url, requestBody, headers: request.headers });
+  console.log("WhatsApp webhook received", {
+    url: request.url,
+    headers: request.headers,
+    requestBody,
+  });
 
   return new Response(JSON.stringify({ success: true }));
 }
