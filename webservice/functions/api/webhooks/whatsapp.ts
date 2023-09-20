35c77ba2d562594bea9acba7242db6788703e1f5
htadotai <144734864+htadotai@users.noreply.github.com>
2023-09-20 00:33:37 +0000

    log webhook request body

--- webservice/functions/api/webhooks/whatsapp.ts
+++ webservice/functions/api/webhooks/whatsapp.ts
@@ -1,10 +1,13 @@
 import { Context, getConfig } from "@/config";
+import { WhatsAppWebhookBody } from "@/lib/whatsapp";
 
 export async function onRequestPost(context: Context) {
   const config = getConfig(context);
   const { request, waitUntil } = context;
 
   // implement stuff here
-  
+  const requestBody = await request.json<WhatsAppWebhookBody>();
+  console.log("WhatsApp webhook received", { requestBody });
+
   return new Response(JSON.stringify({ success: true }));
 }
