66b7a2fe92a2cfba6df507f876dc238c8b90dc4c
HTA <144734864+htadotai@users.noreply.github.com>
2023-09-26 21:57:08 -0700

    add back prompt for transcription

--- webservice/functions/api/webhooks/whatsapp.ts
+++ webservice/functions/api/webhooks/whatsapp.ts
@@ -4,11 +4,9 @@ import { WhatsAppWebhookBody, processWhatsAppWebhook } from "@/lib/whatsapp";
 export async function onRequestGet(context: Context) {
   const config = getConfig(context);
   const { searchParams } = new URL(context.request.url);
-
   let mode = searchParams.get("hub.mode");
   let token = searchParams.get("hub.verify_token");
   let challenge = searchParams.get("hub.challenge");
-
   if (mode === "subscribe" && token === config.WHATSAPP_WEBHOOK_SECRET) {
     return new Response(challenge, { status: 200 });
   }
@@ -18,10 +16,8 @@ export async function onRequestGet(context: Context) {
 export async function onRequestPost(context: Context) {
   const config = getConfig(context);
   const { request, waitUntil } = context;
-
   const requestBody = await request.json<WhatsAppWebhookBody>();
   const requestValue = requestBody.entry?.[0]?.changes?.[0]?.value;
-
   if (!requestBody.object || !requestValue) {
     return new Response("Bad Request", { status: 400 });
   } else {
